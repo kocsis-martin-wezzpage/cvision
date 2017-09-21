e4691986616fafacaaefd4049caba921bc25d5cb
Martin Kocsis <egykettoharo@gmail.com>
2017-09-21 21:59:48 +0200

    config fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
     <meta name="description" content="C-vision">
     <meta name="author" content="C-vision">
 
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/helper.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/public/css/helper.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="Assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/Assets/font-awesome/css/font-awesome.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
